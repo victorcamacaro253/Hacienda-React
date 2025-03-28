b1c4ee55e0d41c82293857a26bcbb8c9576ce9d2
victor <victorcamacaro253@gmail.com>
2025-03-27 22:33:27 -0400

    27/3/25

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import TestimonialsSection from './components/Testimonials';
 import ImageCarousel from './components/ImageCarousel';
 import BreedsShowcase from './components/breedshowcase';
 import BreedsDetails from './breedDetails';
+import Footer from './components/Footer'; // Import the Footer component
+
 
 const App = () => {
   
@@ -32,6 +34,7 @@ const App = () => {
 
 
       </Routes>
+      <Footer/>
     </Router>
   );
 }
